c2416f29b17897fc62bfa76c449bb0ec523de5a2
eduard-martinez <edfemagonza@gmail.com>
2020-08-26 00:57:13 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # clase_3
+
+
+## 1. Descargar la clase
+
+Para descargar las clases debes seguir los siguientes pasos:
+
+```{r}
+# 1. Establecer el directorio de trabajo en el que quieres descargar la carpeta
+setwd("~/Downloads")
+
+# 2. Descargar el repositorio
+download.file(url = "https://github.com/taller-R/clase_3/archive/master.zip", 
+              destfile = "clase_3.zip")
+
+# 3. Descomprimir las carpeta
+unzip(zipfile = "clase_3.zip")
+
+# 4. Cambiar nuevamente el directorio de trabajo
+setwd("~/Downloads/clase_3-master")
+
+# 5. Inspeccionar archivos en el directorio 
+list.files()
+```
+O puede seguir estas [instruciones](https://eduard-martinez.github.io/blog/github/clonar_github.html) para clonar este repositorio en su cuenta de GitHub.  
+
+
+
+## 2. Instalar librerias de la clase
+
+Despues de descargar o clonar el repositorio de la clase, usted debe instalar las siguientes librerias:
+
+```{r}
+install.packages('tidyverse')
+install.packages('readxl') 
+install.packages('haven')
+```
+
+Ahora debe abrir al archivo **Clase 3.html** de la carpeta **help** y el archivo **Clase 3.R** de la carpeta **codes**.
+
+## 3. Notas
+* Los vídeos de la clase se encuentran [aquí](https://www.dropbox.com/sh/49dw2absf405pty/AAD5aPP82qmwjUuy_zzOOADra?dl=0). 
+
+* Por favor hacer todas las correcciones ortográficas a este y los demas archivos .Rmd del repositorio.
---
+++ codes/Clase 3.R
@@ -0,0 +1,213 @@
+# Elaborado por: Eduard F. Martinez Gonzalez
+# Fecha: 5 de febrero de 2020
+# Nota: No se usan acentos ni caracteres especiales para evitar conflictos entre los diferentes sistemas operativos 
+
+
+#--------------------------#
+# 0. Configuración inicial #
+#--------------------------#
+
+#### 0.1 Limpiar la consola, el entorno y fijar directorio de trabajo
+cat("\f")
+rm(list=ls())
+setwd("~/Dropbox/teaching/Taller de R/GitHub/Clases/Clase 3/") # Cambiar este directorio
+
+#### 0.2 Instalar las librerias que vamos a usar en la clase de hoy
+for ( paquete in c('tidyverse','readxl','haven') ){
+      existe = length(grep(paquete,installed.packages()[,1]))
+      if (existe == 0 ){ install.packages(paquete)}
+      else { print(paste0('La libreria ' , paquete , ' ya esta instalada.'))}
+      rm(paquete,existe)
+}
+
+#### 0.3 Llamar las librerias
+library('tidyverse') ; library('readxl') ; library('haven')
+
+#------------------------------------------------------#
+# 1. Importar bases de datos desde diferentes formatos #
+#------------------------------------------------------#
+
+
+#### 1.1 Inspeccionemos las bases de datos en nuestro directorio de trabajo
+list.files("./datos/originales/")
+
+
+#### 1.2.1 Importar bases de datos en formato .csv
+?read.csv
+data_csv <- read.csv2(file = 'datos/originales/censo 2018.csv' ,sep = ",", header = TRUE , stringsAsFactors = F)
+str(data_csv)
+head(data_csv)
+
+
+#### 1.2.2 Importar bases de datos en formato .xls y .xlsx (https://www.policia.gov.co/grupo-información-criminalidad/estadistica-delictiva)
+excel_sheets(path = 'datos/originales/hurto-personas-2020_0.xlsx') # Hojas que contiene el excel 
+?read_excel
+data_xls <- read_excel(path = 'datos/originales/hurto-personas-2020_0.xlsx' , sheet = "Sheet1" , col_names = TRUE) # Cargar archivo
+str(data_xls) 
+head(data_xls)
+
+
+#### 1.2.3 Importar bases de datos en formato .dta
+?read_dta
+data_dta <- read_dta(file = 'datos/originales/Area - Caracteristicas generales (Personas).dta') 
+str(data_dta)
+head(data_dta)
+
+
+#### 1.2.4 Importar bases de datos en formato .rds
+?readRDS
+data_rds = readRDS(file = 'datos/originales/proyecciones DANE 2005-2020.rds')
+str(data_rds) 
+head(data_rds)
+
+
+#### 1.2.5 Importar bases de datos en formato .Rdata
+?load
+load(file = 'datos/originales/Homicidios 2020.Rdata')
+str(data_rdata)
+head(data_rdata)
+
+
+#--------------------------------------------#
+# 2. Filtrar dataframes (filas y/o columnas) #
+#--------------------------------------------#
+
+# Funcion %>% (Pipe)
+c("2","4") %>% as.numeric(.)
+c("4","25") %>% as.numeric() %>% sqrt() %>% + 2  
+sqrt( as.numeric( c("4","25") ) ) + 2  # Esto es equivalente a la linea anterior
+  
+
+#### 2.1.1 Seleccionar columnas de un dataframe usando los nombre de las variables
+colnames(data_rdata)
+
+data_rdata_1 = dplyr::select(data_rdata , municipio , `codigo dane` , mes , cantidad) # usar `` cuando hay espacios en los nombres
+
+data_rdata_2 = dplyr::select(data_rdata , -departamento) # Anteponer el - cuando quiero eliminar una variable
+
+data_rdata_3 = data_rdata[,c('municipio' , 'codigo dane' , 'mes' , 'cantidad')] # Usando el vector de los nombres
+
+
+#### 2.1.2 Seleccionar columnas de un dataframe usando la posicion de las columnas
+colnames(data_rdata)
+
+colnames(data_rdata)[c(2,3,9)]
+
+data_rdata_3 = data_rdata[,c(2,3,9)] # Usando el vector de los nombres
+
+
+#### 2.1.2.1 Veamos la funcion grep
+grep(pattern = 'la' , x = c('Hola','Pola','Nada','Todo'))
+
+grep(pattern = 'municipio' , x = colnames(data_rdata))
+
+nombres = c( grep(pattern = 'municipio' , x = colnames(data_rdata)) , 
+             grep(pattern = 'codigo dane' , x = colnames(data_rdata)) , 
+             grep(pattern = 'mes' , x = colnames(data_rdata)) , 
+             grep(pattern = 'cantidad' , x = colnames(data_rdata))
+            )
+
+
+#### 2.1.2.2 Usemos el vector con la posicion de los nombres
+data_rdata_4 = data_rdata[,nombres]
+
+
+#### 2.1.2.3 Limpiemos el entorno de los nuevos objetos que creamos
+rm(data_rdata_1,data_rdata_2,data_rdata_3,data_rdata_4)
+
+'Revisen en casa esta otra forma de eliminar los objetos que empiezan por data_rdata_ usando la funcion grep'
+ls()
+grep('data_rdata_',ls())
+ls()[grep('data_rdata_',ls())]
+rm(list = ls()[grep('data_rdata_',ls())])
+
+
+#### 2.2 Filtrar filas de un dataframe
+'Vamos a sellecionar solo algunas columnas del dataframe data_csv'
+View(data_csv)
+data_csv = data_csv[,c(2,4,7)]
+colnames(data_csv) = c('cod_dane','name_muni','poblacion')
+view(data_csv)
+
+
+#### 2.2.1 Usando la posicion de las filas
+nrow(data_csv)
+nrow(data_csv)-3
+41:(nrow(data_csv)-3)
+data_csv_1 = data_csv[41:(nrow(data_csv)-3),]
+
+
+#### 2.2.2 Usando los atributos de la variable
+data_csv_2 = subset(x = data_csv, subset = is.na(name_muni) == F)
+
+data_csv_3 = subset(x = data_csv, subset = is.na(name_muni) == F  & as.numeric(poblacion) > 100000)
+
+data_csv_4 = subset(x = data_csv, subset = is.na(name_muni) == F) %>% 
+             subset(as.numeric(poblacion) > 100000)
+
+data_csv_5 = dplyr::filter(data_csv, name_muni %in% c('Medellín','Barranquilla','Cartagena'))
+
+#### 2.2.3 Usando un vector
+is.na(data_csv$name_muni) 
+data_csv_6 = data_csv[is.na(data_csv$name_muni) == F, ]
+
+data_csv_7 = data_csv[ data_csv$name_muni %in% c('Medellín','Barranquilla','Cartagena') , ]
+
+
+#### 2.2.4 Limpiemos la memoria una vez mas
+rm(list = ls()[grep('data_csv_',ls())])
+
+
+#---------------------------#
+# 3 "Colapsar" un dataframe #
+#---------------------------#
+
+#### 3.1 Calculos por grupos
+'primero aseguremonos que el factor de expancion sea numerico'
+is.numeric(data_dta$fex_c_2011)
+
+'numero de encuestados por sexo'
+data_dta %>% group_by(P6020) %>% summarise(total = sum(fex_c_2011))
+
+'numero de encuestados por sexo y ciudad'
+sexo_area = data_dta %>% group_by(P6020,area) %>% summarise(total = sum(fex_c_2011))
+
+#### 3.2 Calculos por grupos aplicando filtros
+
+'media de la educacion y la edad por sexo'
+data_dta %>% group_by(P6020) %>% summarise(education = weighted.mean(ESC, fex_c_2011) ,  age = weighted.mean(P6040, fex_c_2011))
+
+'limpiemos la variable primero'
+is.numeric(data_dta$ESC)
+data_dta %>% subset(is.na(ESC)==F) %>% group_by(P6020) %>% summarise(education = weighted.mean(ESC, fex_c_2011) , 
+                                                                     age = weighted.mean(P6040, fex_c_2011))
+
+'Hagamos el calculo solo para bogota'
+data_dta %>% subset(is.na(ESC)==F & area == 11) %>% group_by(P6020) %>% summarise(education = weighted.mean(ESC, fex_c_2011) , 
+                                                                                  age = weighted.mean(P6040, fex_c_2011))
+
+
+#-----------------------------#
+# # 4 Exportar bases de datos #
+#-----------------------------#
+
+#### 4.1.1 Exportar bases de datos en formato .csv
+?write.csv
+write.csv(x = data_csv , file = 'datos/procesados/censo 2018.csv')
+
+#### 4.1.2 Exportar bases de datos en formato .xls y .xlsx 
+'deben instalar el paquete WriteXLS, pero puede tener un par de problemas.'
+WriteXLS::WriteXLS(x = data_xls, ExcelFileName = "datos/procesados/Hurtos 2020.xlsx" , SheetNames =  "Hurtos") 
+
+#### 4.1.3 Exportar bases de datos en formato .dta
+?write_dta
+write_dta(data = data_dta ,path = 'datos/procesados/Area - Caracteristicas generales (Personas).dta') 
+
+#### 4.1.4 Exportar bases de datos en formato .rds
+?saveRDS
+saveRDS(object = data_rds, file = 'datos/procesados/proyecciones DANE 2005-2020.rds')
+
+#### 4.1.5 Exportar bases de datos en formato .Rdata
+?save
+save(data_rds,data_dta,data_xls,data_csv,data_rdata,file = 'datos/procesados/Datos.Rdata')
+
